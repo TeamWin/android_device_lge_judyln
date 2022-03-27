1c52868d6cd8de381aaa80f670fb1221e2faf758
HanCF <han.cf@foxmail.com>
2022-03-27 12:37:00 +0800

    Upload Files

---
+++ device.mk
@@ -0,0 +1,39 @@
+
+LOCAL_PATH := device/lge/judyln
+
+# Define hardware platform
+PRODUCT_PLATFORM := sdm845
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)/bootctrl \
+    $(LOCAL_PATH)/gpt-utils
+
+# A/B support
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_HOST_PACKAGES += \
+    brillo_update_payload
+
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# Enable update engine sideloading by including the static version of the
+# boot_control HAL and its dependencies.
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    libgptutils \
+    libz \
+    libcutils
+
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    hwservicemanager
---
+++ recovery/root/sbin/prepdecrypt.sh
@@ -0,0 +1,55 @@
+#!/sbin/sh
+
+relink()
+{
+	/sbin/cp $1 ${1}_old
+	sed 's|/system/bin/linker64|///////sbin/linker64|' "${1}_old" > "$1"
+	chmod 755 $1
+}
+
+finish()
+{
+	umount /v
+	umount /s
+	rmdir /v
+	rmdir /s
+
+	relink /sbin/qseecomd
+	relink /sbin/android.hardware.gatekeeper@1.0-service-qti
+	relink /sbin/android.hardware.keymaster@3.0-service-qti
+	relink /sbin/android.hardware.keymaster@4.0-service-qti
+	relink /sbin/strace
+
+	setprop crypto.ready 1
+	exit 0
+}
+
+suffix=$(getprop ro.boot.slot_suffix)
+if [ -z "$suffix" ]; then
+	suf=$(getprop ro.boot.slot)
+	suffix="_$suf"
+fi
+venpath="/dev/block/bootdevice/by-name/vendor$suffix"
+mkdir /v
+mount -t ext4 -o ro "$venpath" /v
+syspath="/dev/block/bootdevice/by-name/system$suffix"
+mkdir /s
+mount -t ext4 -o ro "$syspath" /s
+
+if [ -f /s/system/build.prop ]; then
+	# TODO: It may be better to try to read these from the boot image than from /system
+	osver=$(grep -i 'ro.build.version.release' /s/system/build.prop  | cut -f2 -d'=')
+	patchlevel=$(grep -i 'ro.build.version.security_patch' /s/system/build.prop  | cut -f2 -d'=')
+	resetprop ro.build.version.release "$osver"
+	resetprop ro.build.version.security_patch "$patchlevel"
+	finish
+else
+	# Be sure to increase the PLATFORM_VERSION in build/core/version_defaults.mk to override Google's anti-rollback features to something rather insane
+	osver=$(getprop ro.build.version.release)
+	patchlevel=$(getprop ro.build.version.security_patch)
+	resetprop ro.build.version.release "$osver"
+	resetprop ro.build.version.security_patch "$patchlevel"
+	finish
+fi
+
+exit 0
